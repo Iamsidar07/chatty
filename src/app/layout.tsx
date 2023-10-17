617c0b62d01e0a4c08629d3aff32275b28c770b9
iamsidar07 <ms8460149@gmail.com>
2023-10-17 13:30:17 +0530

    Add: Syne font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Syne } from "next/font/google";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
 import { Toaster } from "@/components/ui/toaster";
 import { constructMetadata } from "@/lib/utils";
 import "simplebar-react/dist/simplebar.min.css";
-const inter = Inter({ subsets: ["latin"] });
+const syne = Syne({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+  variable: "--font-syne",
+});
 
 export const metadata = constructMetadata();
 export default function RootLayout({
@@ -17,9 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="light">
       <Providers>
-        <body
-          className={cn("min-h-screen grainy antialiased", inter.className)}
-        >
+        <body className={cn("min-h-screen grainy antialiased", syne.className)}>
           <Toaster />
           <Navbar />
           {children}
